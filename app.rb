9881f47c8128c7fc7ca9ccc5421f7b45f2df15e9
Nícolas Iensen <nicolasiensen@Nicolas-Iensens-MacBook.local>
2011-03-10 15:37:14 -0300

    New gems: rack-flash and pony. Contact form is working with feedback messages

--- app.rb
+++ app.rb
@@ -1,11 +1,33 @@
 #coding:utf-8
-
 require "sinatra"
 require "compass"
 require "twitter"
+require 'rack-flash'
+require 'pony'
+
+enable :sessions
+use Rack::Flash
 
 get "/" do
   haml :index
+end
+
+post "/" do
+  if(params[:nome] != "Nome" && params[:email] != "E-mail" && params[:mensagem] != "Mensagem")
+    Pony.mail(:to => 'nicolas@quavio.com.br', :via => :smtp, :via_options => {
+      :address              => 'smtp.gmail.com',
+      :port                 => '587',
+      :enable_starttls_auto => true,
+      :user_name            => 'noreplysite@quavio.com.br',
+      :password             => '20110310',
+      :authentication       => :plain,
+      :domain               => "quavio.com.br"
+    })
+    flash[:notice] = "Maravilha, logo entraremos em contato! Abraços!"
+  else
+    flash[:alert] = "Ooops.. Por favor, preencha todos os campos, obrigado!"
+  end
+  redirect "/#servicos_contato"
 end
 
 get '/stylesheets/:name.css' do
